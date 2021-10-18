cc6c63b59c5b46db8ab6a64325b29dda8004d57f
Mariano Perin <mariano.perin@gmail.com>
2021-10-18 19:32:07 -0300

    agregado carpeta containers y reruteos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer.js';
+import ItemListContainer from './components/containers/ItemListContainer/ItemListContainer.js';
 import NavBar from './components/NavBar/NavBar.js';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <ItemListContainer greeting="ACA IRIA EL CATALOGO DE ARTICULOS"/>
+      <ItemListContainer greeting="ACA IRIA EL CATALOGO DE ARTICULOS" />
     </div>
   );
 }
